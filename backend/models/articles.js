c18043b4835450e6d1155aedb59e9a0f7555a1ee
P.R. Neider-Ball <phillrballn@icloud.com>
2025-08-05 10:00:39 -0500

    moved backend to seperate file

--- backend/models/articles.js
+++
@@ -1,9 +0,0 @@
-import mongoose from 'mongoose';
-
-const articleSchema = new mongoose.Schema({
-  title: { type: String, required: true },
-  body: { type: String, required: true },
-  tags: { type: [String], default: [] }
-}, { timestamps: true });
-
-export default mongoose.model('Article', articleSchema);
